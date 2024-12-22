47c2a43bcd28d31a00ef56c1ebf34d5d7a5192db
Marius Muntean <mmarius75@gmail.com>
2024-12-22 11:07:44 +0000

    Update AuthorsForm

--- AuthorsForm.Designer.cs
+++ AuthorsForm.Designer.cs
@@ -45,27 +45,30 @@
             // 
             this.nameLabel.AutoSize = true;
             this.nameLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameLabel.Location = new System.Drawing.Point(12, 9);
+            this.nameLabel.Location = new System.Drawing.Point(9, 7);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(59, 24);
+            this.nameLabel.Size = new System.Drawing.Size(94, 19);
             this.nameLabel.TabIndex = 0;
-            this.nameLabel.Text = "Name";
+            this.nameLabel.Text = "Author Name";
             // 
             // nameTextBox
             // 
             this.nameTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameTextBox.Location = new System.Drawing.Point(12, 36);
+            this.nameTextBox.Location = new System.Drawing.Point(9, 29);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(396, 32);
+            this.nameTextBox.Size = new System.Drawing.Size(298, 27);
             this.nameTextBox.TabIndex = 1;
             // 
             // countryLabel
             // 
             this.countryLabel.AutoSize = true;
             this.countryLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.countryLabel.Location = new System.Drawing.Point(414, 9);
+            this.countryLabel.Location = new System.Drawing.Point(310, 7);
+            this.countryLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.countryLabel.Name = "countryLabel";
-            this.countryLabel.Size = new System.Drawing.Size(77, 24);
+            this.countryLabel.Size = new System.Drawing.Size(59, 19);
             this.countryLabel.TabIndex = 2;
             this.countryLabel.Text = "Country";
             // 
@@ -73,53 +76,59 @@
             // 
             this.countryComboBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.countryComboBox.FormattingEnabled = true;
-            this.countryComboBox.Location = new System.Drawing.Point(414, 36);
+            this.countryComboBox.Location = new System.Drawing.Point(310, 29);
+            this.countryComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.countryComboBox.Name = "countryComboBox";
-            this.countryComboBox.Size = new System.Drawing.Size(170, 32);
+            this.countryComboBox.Size = new System.Drawing.Size(128, 27);
             this.countryComboBox.TabIndex = 3;
             // 
             // birthYearLabel
             // 
             this.birthYearLabel.AutoSize = true;
             this.birthYearLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.birthYearLabel.Location = new System.Drawing.Point(586, 9);
+            this.birthYearLabel.Location = new System.Drawing.Point(440, 7);
+            this.birthYearLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.birthYearLabel.Name = "birthYearLabel";
-            this.birthYearLabel.Size = new System.Drawing.Size(92, 24);
+            this.birthYearLabel.Size = new System.Drawing.Size(72, 19);
             this.birthYearLabel.TabIndex = 4;
             this.birthYearLabel.Text = "Birth Year";
             // 
             // birthYearTextBox
             // 
             this.birthYearTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.birthYearTextBox.Location = new System.Drawing.Point(594, 36);
+            this.birthYearTextBox.Location = new System.Drawing.Point(446, 29);
+            this.birthYearTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.birthYearTextBox.Name = "birthYearTextBox";
-            this.birthYearTextBox.Size = new System.Drawing.Size(100, 32);
+            this.birthYearTextBox.Size = new System.Drawing.Size(76, 27);
             this.birthYearTextBox.TabIndex = 5;
             // 
             // deceaseYearLabel
             // 
             this.deceaseYearLabel.AutoSize = true;
             this.deceaseYearLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deceaseYearLabel.Location = new System.Drawing.Point(696, 9);
+            this.deceaseYearLabel.Location = new System.Drawing.Point(522, 7);
+            this.deceaseYearLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.deceaseYearLabel.Name = "deceaseYearLabel";
-            this.deceaseYearLabel.Size = new System.Drawing.Size(119, 24);
+            this.deceaseYearLabel.Size = new System.Drawing.Size(97, 19);
             this.deceaseYearLabel.TabIndex = 6;
             this.deceaseYearLabel.Text = "Decease Year";
             // 
             // deceaseYearTextBox
             // 
             this.deceaseYearTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deceaseYearTextBox.Location = new System.Drawing.Point(700, 36);
+            this.deceaseYearTextBox.Location = new System.Drawing.Point(525, 29);
+            this.deceaseYearTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.deceaseYearTextBox.Name = "deceaseYearTextBox";
-            this.deceaseYearTextBox.Size = new System.Drawing.Size(115, 32);
+            this.deceaseYearTextBox.Size = new System.Drawing.Size(87, 27);
             this.deceaseYearTextBox.TabIndex = 7;
             // 
             // addButton
             // 
             this.addButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addButton.Location = new System.Drawing.Point(12, 74);
+            this.addButton.Location = new System.Drawing.Point(9, 60);
+            this.addButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(75, 35);
+            this.addButton.Size = new System.Drawing.Size(56, 28);
             this.addButton.TabIndex = 8;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -128,9 +137,10 @@
             // closeButton
             // 
             this.closeButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.closeButton.Location = new System.Drawing.Point(734, 163);
+            this.closeButton.Location = new System.Drawing.Point(550, 132);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(85, 38);
+            this.closeButton.Size = new System.Drawing.Size(64, 31);
             this.closeButton.TabIndex = 9;
             this.closeButton.Text = "Close";
             this.closeButton.UseVisualStyleBackColor = true;
@@ -141,17 +151,18 @@
             this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.statusLabel.AutoSize = true;
             this.statusLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLabel.Location = new System.Drawing.Point(12, 177);
+            this.statusLabel.Location = new System.Drawing.Point(9, 144);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(62, 24);
+            this.statusLabel.Size = new System.Drawing.Size(49, 19);
             this.statusLabel.TabIndex = 10;
             this.statusLabel.Text = "Status";
             // 
             // AuthorsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(831, 213);
+            this.ClientSize = new System.Drawing.Size(623, 173);
             this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.addButton);
@@ -163,6 +174,7 @@
             this.Controls.Add(this.countryLabel);
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.nameLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AuthorsForm";
             this.Text = "AddAuthorForm";
             this.ResumeLayout(false);
